fae3b4ce81348c880fcf72491e854150e5b6e496
Fernando Handal <fernando.handal@usach.cl>
2018-03-21 16:47:03 -0300

    commit de prueba

--- archivo3.py
+++ archivo3.py
@@ -1,4 +1,4 @@
- ##Introducir dos valores A y B:
+##Introducir dos valores A y B:
 ###Si A>=B, calcular e imprimir la suma 10+14+18+...+50 
 ###Si A/B<=30, calcular e imprimir el valor de (A^2+B^2)
 a = input('Primer valor: ')
