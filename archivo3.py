2e87a3af1f08e98719a27d120a3fadddc678cc6a
Fernando Handal <fernando.handal@usach.cl>
2018-03-22 17:00:16 -0300

    commit de prueba

--- archivo3.py
+++ archivo3.py
@@ -14,17 +14,3 @@ if a >= b:
 if a/b <= 30:
     sumas = (a**2+b**2)
 print sumas
-
-a = input('Primer valor: \n')
-b = input('Segundo valor: \n')
-n = 10
-suma = 0
-sumas = 0
-if a >= b:
-    while n <= 50:
-        suma += n
-        n += 4
-    print suma
-if a/b <= 30:
-    sumas = (a**2+b**2)
-print sumas
