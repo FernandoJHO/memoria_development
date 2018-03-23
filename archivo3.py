dae626b023fea8dce13d3d64094fd6aa528ed4de
Fernando Handal <fernando.handal@usach.cl>
2018-03-23 19:47:37 -0300

    commit de prueba

--- archivo3.py
+++ archivo3.py
@@ -1,69 +1,27 @@
-def Menu():
+# -*- coding: utf-8 -*-
 
-    """Funcion que Muestra el Menu"""
+ 
 
-    print """************
+# define la función
 
-Calculadora
+def mcd(a, b):
 
-************
+	resto = 0
 
-Menu
+	while(b > 0):
 
-1) Suma
+		resto = b
 
-2) Resta
+		b = a % b
 
-3) Multiplicacion
+		a = resto
 
-4) Division
+	return a
 
-5) Salir"""
+num1 = int(input("Introduce el primer numero: \n"))
 
-def Calculadora():
+num2 = int(input("Introduce el segundo numero: \n"))
 
-    """Funcion Para Calcular Operaciones Aritmeticas"""
+ 
 
-    Menu()
-
-    opc = int(input("Selecione Opcion\n"))
-
-    while (opc >0 and opc <5):
-
-        x = int(input("Ingrese Numero\n"))
-
-        y = int(input("Ingrese Otro Numero\n"))
-
-        if (opc==1):
-
-            print "La Suma es:", x+y
-
-            opc = int(input("Selecione Opcion\n"))
-
-        elif(opc==2):
-
-            print "La Resta es:",x-y
-
-            opc = int(input("Selecione Opcion\n"))
-
-        elif(opc==3):
-
-            print "La Multiplicacion es:",x*y
-
-            opc = int(input("Selecione Opcion\n"))
-
-        elif(opc==4):
-
-            try:
-
-              print "La Division es:", x/y
-
-              opc = int(input("Selecione Opcion\n"))
-
-            except ZeroDivisionError:
-
-              print "No se Permite la Division Entre 0"
-
-              opc = int(input("Selecione Opcion\n"))
-
-Calculadora()
+print "El máximo común divisor de ", num1," y ", num2," es ", mcd(num1, num2)
