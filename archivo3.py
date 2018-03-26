680d6604313dfb5edebab33e6b54c743e690d451
Fernando Handal <fernando.handal@usach.cl>
2018-03-26 16:35:49 -0300

    commit de prueba

--- archivo3.py
+++ archivo3.py
@@ -1,1 +1,27 @@
-print "hola"
+# -*- coding: utf-8 -*-
+
+ 
+
+# define la función
+
+def mcd(a, b):
+
+	resto = 0
+
+	while(b > 0):
+
+		resto = b
+
+		b = a % b
+
+		a = resto
+
+	return a
+
+num1 = int(input("Introduce el primer numero: \n"))
+
+num2 = int(input("Introduce el segundo numero: \n"))
+
+ 
+
+print "El máximo común divisor de ", num1," y ", num2," es ", mcd(num1, num2)
